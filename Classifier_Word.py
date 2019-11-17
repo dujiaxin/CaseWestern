6b61002806f417346d1f0f5bb9b096f142163aa9
dujiaxin <fashui01@163.com>
2019-11-17 17:09:43 -0500

    gluonnlp glove

--- Classifier_Word.py
+++ Classifier_Word.py
@@ -1,50 +1,34 @@
-import bcolz
+from tqdm import tqdm
 import numpy as np
-import pickle
+from joblib import dump, load
 import json
 import nltk
+import mxnet as mx
+import gluonnlp as nlp
+import random
+from sklearn import svm
 
 if __name__ == '__main__':
+    random.seed(1)
+    np.random.seed(1)
+    mx.random.seed(1)
 
-    words = []
-    idx = 0
-    word2idx = {}
-    vectors = bcolz.carray(np.zeros(1), rootdir='./data/glove.840B.300d.dat', mode='w')
+    glove = nlp.embedding.create('glove', source='glove.840B.300d', embedding_root='./data/')
 
-    with open('./data/glove.840B.300d.txt', 'rb') as f:
-        for l in f:
-            line = l.decode().split()
-            word = line[0]
-            words.append(word)
-            word2idx[word] = idx
-            idx += 1
-            if line[0] == '.':
-                vect = np.array(line[3:]).astype(np.float)
-            else:
-                vect = np.array(line[1:]).astype(np.float)
-            vectors.append(vect)
-
-    vectors = bcolz.carray(vectors[1:].reshape((2196017, 300)), rootdir='./data/glove.840B.300d.dat', mode='w')
-    vectors.flush()
-    pickle.dump(words, open('./data/glove.840B.300d_words.pkl', 'wb'))
-    pickle.dump(word2idx, open('./data/glove.840B.300d_idx.pkl', 'wb'))
-
-    vectors = bcolz.open('./data/glove.840B.300d.dat')[:]
-    words = pickle.load(open('./data/glove.840B.300d_words.pkl', 'rb'))
-    word2idx = pickle.load(open('./data/glove.840B.300d_idx.pkl', 'rb'))
-
-    glove = {w: vectors[word2idx[w]] for w in words}
-
-    word_embeddings = []
+    word_embeddings = mx.ndarray.empty((300,))
     labels = []
+    docs = []
     with open('./data/input.json', 'r') as f:
         trainloader = json.load(f)
-        for i in trainloader:
+        for i, items in enumerate(tqdm(trainloader[:10])):
             doc = []
-            tokens = nltk.word_tokenize(i['document'].lower())
-            for ii in tokens:
-                doc.append(glove(ii))
-            doc_mean = np.ndarray(doc).mean(0)
-            word_embeddings.append((doc_mean, i['is_credible']))
-
-    all = np.ndarray(word_embeddings)
+            tokens = nltk.word_tokenize(items['document'].lower())
+            for ii, word in enumerate(tokens, 10):
+                word_embeddings = mx.ndarray.concat(word_embeddings, glove[word], dim=0)
+            doc_mean = word_embeddings.reshape(len(tokens), 300).mean(axis=0)
+            docs.append(doc_mean.asnumpy())
+            labels.append(items['is_credible'])
+    clf = svm.SVC()
+    clf.fit(docs, labels)
+    dump(clf, './model/svm_glove_word_mean.joblib')
+    print(clf.predict(docs[0]))

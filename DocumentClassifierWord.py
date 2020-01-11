ce596d4ef88777aa02f9be541759af81d2330c62
JiaxinDu <jd688@njit.edu>
2020-01-11 13:08:59 -0500

    Bag of words + Naive Bayes

--- DocumentClassifierWord.py
+++ DocumentClassifierWord.py
@@ -29,12 +29,6 @@ def splite_file(text_filepath, to_filepath):
         print('neg_count : ' + str(neg_count))
         return train_data, content_pos, content_neg
 
-def lexical_diversity(text):
-    return len(set(text)) / len(text)
-
-def percentage(count, total):
-    return 100 * count / total
-
 
 def unusual_words(text):
     text_vocab = set(w.lower() for w in text if w.isalpha())
@@ -46,34 +40,49 @@ def unusual_words(text):
 def replace_marks(string, maxsplit=0):
     # replace special marks
     # string.replace('\\n','').replace('\\t','')
-    markers = "*", "/"
+    markers = "*", "/", "+"
     regexPattern = '|'.join(map(re.escape, markers))
     return re.sub(regexPattern, ' ', string)
 
 
-def document_features(document,word_features):
+def document_features(document,word_features, ps=None):
     document_words = set(document)
     features = {}
     for word in word_features:
-        features['contains({})'.format(word)] = (word in document_words)
+        if ps == None:
+            features['contains({})'.format(word)] = (word in document_words)
+        else:
+            features['contains({})'.format(word)] = (ps.stem(word) in document_words)
     return features
 
 
 def main():
-    random.seed(1)
+    random.seed(0)
     nltk.download('punkt')
     nltk.download('words')
     text_filepath = './docs.json'
     to_filepath = './content/all.json'
-    content = ''
-    #content = prepare_file(text_filepath, to_filepath)
+    ps = PorterStemmer()
+
+    stopwords_file = './stopwords.txt'
+    default_stopwords = set(nltk.corpus.stopwords.words('english'))
+    with open(stopwords_file, 'r', encoding='utf-8') as f:
+        custom_stopwords = set(f.read().splitlines())
+    all_stopwords = default_stopwords | custom_stopwords
+
     train_data, content_pos, content_neg = splite_file(text_filepath, to_filepath)
     tokens_pos = word_tokenize(replace_marks(content_pos.lower()))
     tokens_neg = word_tokenize(replace_marks(content_neg.lower()))
+    feature_set = set(tokens_neg + tokens_pos)
+    # Remove single-character tokens (mostly punctuation)
+    words = [word for word in feature_set if len(word) > 1]
+    # Remove numbers
+    # words = [word for word in tokens if not word.isnumeric()]
+    feature_set_stem_nostop = set(w for w in words if w not in all_stopwords)
     documents = [(word_tokenize(replace_marks(category['document'].lower())), category['credible_issue'])
                      for category in train_data]
     random.shuffle(documents)
-    featuresets = [(document_features(d, set(tokens_neg+tokens_pos)), c) for (d, c) in documents]
+    featuresets = [(document_features(d, feature_set_stem_nostop), c) for (d, c) in documents]
     train_set, test_set = featuresets, featuresets
     classifier = nltk.NaiveBayesClassifier.train(train_set)
     print(nltk.classify.accuracy(classifier, test_set))

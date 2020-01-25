0434c32d07838da793f84dce7a5a20e3a56ef18c
Matthew Kobilas <mattkobi789@gmail.com>
2020-01-25 14:47:29 -0500

    Trying out different grammar checking package
    https://github.com/myint/language-check

---
+++ languagecheck.py
@@ -0,0 +1,11 @@
+import sys
+import os
+import csv
+import docx2txt as d2t
+from docx import Document
+from natsort import natsorted
+# pip install pyspellchecker
+from spellchecker import SpellChecker
+# pip install language-check
+import language_check 
+

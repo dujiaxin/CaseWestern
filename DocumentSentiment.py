0a04e9cf06593b9c868c0360fb57fbe8732a4b25
JiaxinDu <jd688@njit.edu>
2020-01-28 09:55:53 -0500

    add sentiment and longtitude and latitude

--- DocumentSentiment.py
+++ DocumentSentiment.py
@@ -1,15 +1,12 @@
 import nltk
-from nltk.tokenize import word_tokenize,RegexpTokenizer
-import os
-import docx
-from tqdm import tqdm
-import re
-from nltk.corpus import stopwords
+import pandas as pd
 import json
 import random
 from nltk.stem.porter import PorterStemmer
 from FeatureClass import FeatureClass
 import sentiment
+from tqdm import tqdm
+
 
 def main():
     random.seed(0)
@@ -17,13 +14,24 @@ def main():
     nltk.download('words')
     nltk.download('averaged_perceptron_tagger')
     nltk.download('wordnet')
-    train_filepath = './labelled_report.json'
+    train_filepath = './train_sak.json'
     # fc = FeatureClass()
     with open(train_filepath, 'r', encoding='utf-8') as ft:
         train_data = json.load(ft)
     sntmnt = sentiment.SentimentAnalysis()
-    for category in train_data:
-        print(sntmnt.score(category['document']))
+    df_labels = pd.read_csv('./Police_reports_geocodings.csv')
+    df_indexed = df_labels.set_index('USER_BCI_N')
+    categories = list(df_indexed.columns.values)
+    for t in tqdm(train_data):
+        t.update({'sentiment': str(sntmnt.score(t['document']))})
+        for category in categories:
+            try:
+                t.update({category : str(df_indexed.loc[t['BCI_Number_Compl']][category])})
+            except KeyError:
+                t.update({category : '999'})
+
+    with open('./train_sak_geo.json', 'w', encoding='utf-8') as fw:
+        json.dump(train_data, fw)
     return 0
 
 

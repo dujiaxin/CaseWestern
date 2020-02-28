ddee2f63292e110f9de983b0c95651bff40b18ca
Matthew Kobilas <mattkobi789@gmail.com>
2020-02-28 18:59:08 -0500

    Added more comments and clear step-by-step process for how this program runs

--- wordcloud_tfidf.py
+++ wordcloud_tfidf.py
@@ -10,9 +10,11 @@ from wordcloud import WordCloud
 import docx2txt as d2t
 from tqdm import tqdm
 
+# blacklisted files not to be tokenized and put into word frequency dictionary
 blacklist = [
     'document_process.csv'
 ]
+# blacklisted words that are nonsensical and not picked up by stopword detector
 blacklist_words = [
     'ptl',
     'lukes',
@@ -29,8 +31,13 @@ blacklist_words = [
 # TEST_SAMPLE is a dir that only contains a dir named CLEANED_2
 # CLEANED_2 only contains a dir named 14
 # which only contains 30 .docx police reports that have been extracted and cleaned
+# file paths to utilize
+# idfDirPath is used to find documents and words to build idf csv from
+# test with 1 dir of 30 items so far
 idfDirPath = 'C:/Users/Matt/Documents/Data Science/CW/TEST_SAMPLE/CLEANED_2'
+# wordCountFilePath is the file path to output the idf csv to
 wordCountFilePath = 'C:/Users/Matt/Documents/GitHub/CaseWestern/wc_wordCountDict.csv'
+# docFilePath is the document that will be used to calculate tf information
 docFilePath = 'C:/Users/Matt/Documents/Data Science/CW/TEST_SAMPLE/CLEANED_2/14/M518--3338_RMS98-67176.docx'
 # irrelevant or overused words
 stopwords = stopwords.words('english') + blacklist_words + [punc for punc in string.punctuation]
@@ -38,15 +45,19 @@ original_corpus = []
 
 print("imported, inited, and now defining methods")
 
+# color function that changes colors of wordcloud depending on the tfidf of the word
 def my_tfidf_color_func(word, font_size, position, orientation, random_state=None, **kwargs):
     return 'hsl(%d, 80%%, 50%%)' % (360 * kwargs[freqDict][word])
 
+# simple function that determines whether or not numbers exist in a string, causing it to return a boolean value
 def hasNumbers(inp):
     for char in inp:
         if char.isdigit():
             return True
     return False
 
+# build idf info from the documents found in the directories at the root CLEANED directory, dirPath
+# not run if there is a prior wc_wordCountDict.csv, program goes straight to extracting information from file
 def buildIdfInfo(dirPath):
     # word : [number of times appearing at least once in file, found in file flag]
     freqDict = {}
@@ -86,17 +97,22 @@ def buildIdfInfo(dirPath):
                 else:
                     if freqDict[tokens[i]][1] == True:
                         freqDict[tokens[i]][0] += 1
+            # clear large amount of mem for at least a small time
             del text
             del tokens
             # resetting the boolean that determines whether or not a word has yet been seen in the document
             for value in freqDict.values():
                 value[1] = True
+    # output all information to filepath dictated at top of program to file wc_wordCountDict.csv
+    # first row contains "file count: <int file count>"
+    # next rows contain "<str word> <int count>"
     with open('wc_wordCountDict.csv', 'w', newline='', encoding='utf-8') as fileDict:
         fileDictWriter = csv.writer(fileDict)
         fileDictWriter.writerow(["file count:", str(filecount)])
         for key, value in freqDict.items():
             fileDictWriter.writerow([key, str(value[0])])
 
+# method used to extract idf info from previously created wc_wordCountDict.csv file
 def extractIdfInfo(filePath, tfDict):
     idfDict = {}
     wordcount = 0
@@ -112,7 +128,7 @@ def extractIdfInfo(filePath, tfDict):
                 idfDict[row[0]] = wordcount / int(row[1])
     return idfDict
 
-
+# method that builds and extracts tf info based on the document selected at the top of this program
 def buildExtractTfInfo(filePath):
     # word : [number of times appearing in file]
     freqDict = {}
@@ -134,10 +150,19 @@ def buildExtractTfInfo(filePath):
     del text
     del tokens
     tfDict = {}
+    # no need to store information since there is not much data being utilized just yet
     for key, value in freqDict.items():
         tfDict[key] = value / wordcount
     return tfDict
 
+# main program run flow can be seen here
+# build idf info
+# build tf info
+# extract tf info
+# extract idf info
+# calculate tfidf
+# create wordcloud
+# recolor wordcloud <- current problem
 def main():
     print("in main()")
     if not path.exists(wordCountFilePath):
@@ -160,6 +185,7 @@ def main():
         width=1024,
         height=720
     ).generate_from_frequencies(tfDict)
+    # TypeError: my_tfidf_color_func() missing 4 required positional arguments: 'word', 'font_size', 'position', and 'orientation'
     wc.recolor(color_func=my_tfidf_color_func(freqDict=tfidfDict))\
 
 main()
